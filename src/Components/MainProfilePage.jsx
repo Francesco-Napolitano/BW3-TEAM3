2829610674c0ee7a453ead6043a912557c91653b
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2024-12-16 14:08:29 +0100

    commit

---
+++ src/Components/MainProfilePage.jsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from 'react-bootstrap'
+
+const MainProfilePage = () => {
+  return (
+    <Container fluid>
+      <Row className="">
+        <Col></Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default MainProfilePage
