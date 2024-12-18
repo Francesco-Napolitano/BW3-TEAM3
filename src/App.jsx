e2c9e7c8f08a9dc58082262f8fb7fb353542b900
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2024-12-18 15:31:03 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,14 @@ import UsersProfilePage from './Components/UsersProfilePage'
 import HomePage from './Components/Homepage'
 import LeftSidebarHome from './Components/LeftSidebarHome'
 import ErrorPage from './Components/ErrorPage'
-<<<<<<< HEAD
 import SavedPosts from './Components/SavedPosts'
-=======
-import JobsPage from './Components/JobsPage'; 
->>>>>>> main
+import JobsPage from './Components/JobsPage'
 
 function App() {
   return (
     <>
       <CustomNavBar />
       <Routes>
-<<<<<<< HEAD
         <Route
           path="/"
           // profile/me da aggiungere
@@ -72,6 +68,8 @@ function App() {
             </Container>
           }
         />
+        <Route path="/jobs" element={<JobsPage />} />
+
         <Route
           path="/favourites"
           element={
@@ -104,73 +102,6 @@ function App() {
           }
         />
       </Routes>
-=======
-  <Route
-    path="/"
-    element={
-      <Container>
-        <Row className="mt-5">
-          <Col xs={12} xl={9}>
-            <MainProfilePage />
-          </Col>
-          <Col xs={3}>
-            <SideBar />
-          </Col>
-        </Row>
-      </Container>
-    }
-  />
-  <Route
-    path="/profile/:id"
-    element={
-      <Container>
-        <Row className="mt-5">
-          <Col xs={12} xl={9}>
-            <UsersProfilePage />
-          </Col>
-          <Col xs={3}>
-            <SideBar />
-          </Col>
-        </Row>
-      </Container>
-    }
-  />
-  <Route
-    path="/homepage"
-    element={
-      <Container>
-        <Row className="mt-5">
-          <Col lg={3}>
-            <LeftSidebarHome />
-          </Col>
-          <Col xs={12} lg={6}>
-            <HomePage />
-          </Col>
-          <Col lg={3}>
-            <SideBar />
-          </Col>
-        </Row>
-      </Container>
-    }
-  />
-  <Route
-    path="/jobs"
-    element={<JobsPage />} 
-  />
-  <Route
-    path="*"
-    element={
-      <Container>
-        <Row className="mt-5">
-          <Col>
-            <ErrorPage />
-          </Col>
-        </Row>
-      </Container>
-    }
-  />
-</Routes>
->>>>>>> main
       <Footer />
     </>
   )
