d3d7bceb81a6fb3632223b974a07a84d9dd964ff
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2024-12-18 14:17:39 +0100

    commit

---
+++ src/Components/SavedPosts.jsx
@@ -0,0 +1,8 @@
+const savedPosts = () => {
+  return (
+    <div>
+      <h1>Saved Posts</h1>
+    </div>
+  )
+}
+export default savedPosts
---
+++ src/redux/reducers/SavedPosts.js
@@ -0,0 +1,10 @@
+const initialState = {
+  list: [],
+}
+
+const SavedPost = (state = initialState, action) => {
+  switch (UserActivation.type) {
+    case 'getSavedPosts':
+      return {}
+  }
+}
