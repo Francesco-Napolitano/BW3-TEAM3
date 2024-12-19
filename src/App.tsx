ee8ce82bbbb8093d787cffcc3d8e297ef11717f8
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2024-12-19 09:53:41 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Provider } from "react-redux";
-import store from "./store/store";
-import Login from "./components/Login";
-import CustomNavBar from "./Components/CustomNavBar";
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from './store/store'
+import Login from './Components/Login'
+import CustomNavBar from './Components/CustomNavBar'
 
 const App: React.FC = () => (
   <Provider store={store}>
-     <CustomNavBar />
+    <CustomNavBar />
     <Login />
   </Provider>
-);
+)
 
-export default App;
+export default App
